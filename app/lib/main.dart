ef952dfd30c7edce177229cde26acf5eaa028704
Drishti <82228411+Drishti9421@users.noreply.github.com>
2023-02-05 05:14:22 +0530

    sjhdfk

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -30,6 +30,6 @@ class MyApp extends StatelessWidget {
         theme: ThemeData(
           primarySwatch: Colors.blue,
         ),
-        home: EcoFriendly());
+        home: EventsPage());
   }
 }
